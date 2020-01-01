0a63af95dbda09b4b1f6892a2781f26ecfe22958
PB2 <gitstuff2@gmail.com>
2019-12-31 20:51:38 -0500

    Add files via upload

--- Autopilot.cpp
+++ Autopilot.cpp
@@ -33,55 +33,40 @@ int centiDeg_to_us(int controller_output_, int servoMin_, int servoMax_)
 
 
 
-void pitch_controller::begin(float setpoint_,
-                             int maxRate_up_,
-                             int maxRate_down_,
-                             float kp_,
-                             float ki_,
-                             float kd_,
-                             float roll_comp_,
-                             float sampleRate_,
-                             int servoMax_,
-                             int servoMin_)
+void pitch_controller::begin(control_params params)
 {
-	update(setpoint_,
-	       maxRate_up_,
-	       maxRate_down_,
-	       kp_,
-	       ki_,
-	       kd_,
-	       roll_comp_,
-	       sampleRate_,
-	       servoMax_,
-	       servoMin_);
+	update(params);
+	reset();
 }
 
 
 
 
-void pitch_controller::update(float setpoint_,
-                              int maxRate_up_,
-                              int maxRate_down_,
-                              float kp_,
-                              float ki_,
-                              float kd_,
-                              float roll_comp_,
-                              float sampleRate_,
-                              int servoMax_,
-                              int servoMin_)
+void pitch_controller::update(control_params params)
 {
-	setpoint        = float_constrain(setpoint_, MIN_SETPOINT, MAX_SETPOINT);
-	maxRate_up      = float_constrain(maxRate_up_, MIN_RATE_UP, MAX_RATE_UP);
-	maxRate_down    = float_constrain(maxRate_down_, MIN_RATE_DN, MAX_RATE_DN);
-	samplePeriod_s  = float_constrain((1 / sampleRate_), MIN_T, MAX_T);
+	setpoint        = float_constrain(params.setpoint, MIN_SETPOINT, MAX_SETPOINT);
+	maxRate_up      = float_constrain(abs(params.maxRate_up), MIN_RATE_UP, MAX_RATE_UP);
+	maxRate_down    = float_constrain(abs(params.maxRate_down), MIN_RATE_DN, MAX_RATE_DN);
+	samplePeriod_s  = 1 / float_constrain(params.sampleRate, MIN_F, MAX_F);
 	samplePeriod_ms = 1000 * samplePeriod_s;
-	kp              = float_constrain(find_kp(kp_, ki_, kd_, samplePeriod_s), MIN_P, MAX_P);
-	ki              = float_constrain(find_ki(ki_, samplePeriod_s), MIN_I, MAX_I);
-	kd              = float_constrain(kd_, MIN_D, MAX_D);
-	roll_comp       = float_constrain(roll_comp_, MIN_ROLL_COMP, MAX_ROLL_COMP);
-	servoMax        = constrain(servoMax_, MIN_SERVO, MAX_SERVO);
-	servoMin        = constrain(servoMin_, MIN_SERVO, MAX_SERVO);
+	kp              = float_constrain(find_kp(params.kp, params.ki, params.kd, samplePeriod_s), MIN_P, MAX_P);
+	ki              = float_constrain(find_ki(params.ki, samplePeriod_s), MIN_I, MAX_I);
+	kd              = float_constrain(params.kd, MIN_D, MAX_D);
+	servoMax        = params.servoMax;
+	servoMin        = params.servoMin;
+}
 
+
+
+
+void pitch_controller::reset()
+{
+	sampleTimer_previous = 0;
+	sampleTimer_current  = 0;
+	sample_time_actual   = 0;
+	error = 0;
+	previousError  = 0;
+	summedError    = 0;
 	sampleTimer_current  = millis();
 	sampleTimer_previous = sampleTimer_current;
 }
@@ -93,36 +78,33 @@ float pitch_controller::compute(float pitchAngle, // Degrees
                                 float rollAngle,  // Degrees
                                 float airspeed)   // m/s
 {
-	float limited_err;
 	float roll_bias;
-	float pitch_rate; // Degrees per Sec
-	float rate_adj;
 	float controller_output;
 
 	sampleTimer_current = millis();
-	sample_time_actual = sampleTimer_current - sampleTimer_previous;
-
+	sample_time_actual  = sampleTimer_current - sampleTimer_previous;
+	
 	if (sample_time_actual >= samplePeriod_ms)
 	{
 		sampleTimer_previous += samplePeriod_ms;
 		
 		previousError = error;
-		error         = setpoint - pitchAngle;
+		error = setpoint - pitchAngle;
 
-		limited_err     = float_constrain(error * omega(), maxRate_down, maxRate_up);
-		roll_bias       = roll_compensation(rollAngle, airspeed);
-		biasedError     = limited_err + roll_bias;
-		pitch_rate      = (pitchAngle - prevPitchAngle) * (sample_time_actual / 1000); // Degrees per Sec
-		rate_adj        = biasedError + pitch_rate;
-		airspeed_scaler = find_as_scaler(airspeed);
-		previous_scaler = scaler_output;
-		scaler_output   = airspeed_scaler * rate_adj;
-
-		p_val = p_component();
+		p_val = constrain(p_component(), -(10 * (long)samplePeriod_ms), 10 * (long)samplePeriod_ms);
 		i_val = i_component();
 		d_val = d_component();
 		
-		controller_output = (p_val + i_val + d_val) * airspeed_scaler;
+		controller_output = constrain((p_val + i_val + d_val) + (18000 / 2), 0, 18000);
+
+		Serial1.println(summedError, 10);
+		Serial1.print("setpoint: "); Serial1.println(setpoint);
+		Serial1.print("previousError: "); Serial1.println(previousError);
+		Serial1.print("error: "); Serial1.println(error);
+		Serial1.print("p_val: "); Serial1.println(p_val);
+		Serial1.print("i_val: "); Serial1.println(i_val);
+		Serial1.print("d_val: "); Serial1.println(d_val);
+		Serial1.println();
 
 		status = true;
 		return centiDeg_to_us(controller_output, servoMin, servoMax);
@@ -135,12 +117,36 @@ float pitch_controller::compute(float pitchAngle, // Degrees
 
 
 
+float pitch_controller::get_P_Component()
+{
+	return p_val;
+}
+
+
+
+
+float pitch_controller::get_I_Component()
+{
+	return i_val;
+}
+
+
+
+
+float pitch_controller::get_D_Component()
+{
+	return d_val;
+}
+
+
+
+
 float pitch_controller::find_kp(float kp_,
                                 float ki_,
                                 float kd_,
                                 float samplePeriod)
 {
-	return (kp_ - (ki_ * samplePeriod)) * (samplePeriod - kd_);
+	return kp_;
 }
 
 
@@ -148,15 +154,7 @@ float pitch_controller::find_kp(float kp_,
 
 float pitch_controller::find_ki(float ki_, float samplePeriod)
 {
-	return ki_ * samplePeriod;
-}
-
-
-
-
-float pitch_controller::omega()
-{
-	return 1000 / samplePeriod_s;
+	return ki_;
 }
 
 
@@ -164,20 +162,7 @@ float pitch_controller::omega()
 
 float pitch_controller::roll_compensation(float rollAngle, float airspeed)
 {
-	float rollAngle_rad = (M_PI / 180) * rollAngle;
-
-	return (roll_comp * GRAVITY) / (airspeed * tan(rollAngle_rad) * sin(rollAngle_rad));
-}
-
-
-
-
-float pitch_controller::find_as_scaler(float airspeed)
-{
-	if (airspeed >= AIRSPEED_THRESHOLD)
-		return 1 / airspeed;
-	else
-		return 2;
+	return 0;
 }
 
 
@@ -185,7 +170,7 @@ float pitch_controller::find_as_scaler(float airspeed)
 
 float pitch_controller::p_component()
 {
-	return kp * biasedError;
+	return kp * error;
 }
 
 
@@ -193,8 +178,8 @@ float pitch_controller::p_component()
 
 float pitch_controller::i_component()
 {
-	summedError += ((error - previousError) / 2) * (sample_time_actual / 1000);
-	summedError  = float_constrain(summedError, -i_limit, i_limit);
+	summedError += ((error + previousError) / 2.0) * (sample_time_actual / 1000.0);
+	summedError  = float_constrain(summedError, -i_limit / ki, i_limit / ki);
 
 	return ki * summedError;
 }
@@ -203,6 +188,157 @@ float pitch_controller::i_component()
 
 
 float pitch_controller::d_component()
+{
+	return kd * (error - previousError) / sample_time_actual;
+}
+
+
+
+
+void roll_controller::begin(control_params params)
+{
+	update(params);
+	reset();
+}
+
+
+
+
+void roll_controller::update(control_params params)
+{
+	setpoint = float_constrain(params.setpoint, MIN_SETPOINT, MAX_SETPOINT);
+	samplePeriod_s = 1 / float_constrain(params.sampleRate, MIN_F, MAX_F);
+	samplePeriod_ms = 1000 * samplePeriod_s;
+	kp = float_constrain(find_kp(params.kp, params.ki, params.kd, samplePeriod_s), MIN_P, MAX_P);
+	ki = float_constrain(find_ki(params.ki, samplePeriod_s), MIN_I, MAX_I);
+	kd = float_constrain(params.kd, MIN_D, MAX_D);
+	servoMax = params.servoMax;
+	servoMin = params.servoMin;
+}
+
+
+
+
+void roll_controller::reset()
+{
+	sampleTimer_previous = 0;
+	sampleTimer_current = 0;
+	sample_time_actual = 0;
+	error = 0;
+	previousError = 0;
+	summedError = 0;
+	biasedError = 0;
+	sampleTimer_current = millis();
+	sampleTimer_previous = sampleTimer_current;
+}
+
+
+
+
+float roll_controller::compute(float rollAngle, // Degrees
+                               float airspeed)  // m/s
+{
+	float controller_output;
+
+	sampleTimer_current = millis();
+	sample_time_actual = sampleTimer_current - sampleTimer_previous;
+
+	if (sample_time_actual >= samplePeriod_ms)
+	{
+		sampleTimer_previous += samplePeriod_ms;
+
+		previousError = error;
+		error = setpoint - rollAngle;
+
+		p_val = p_component();
+		i_val = i_component();
+		d_val = d_component();
+
+		controller_output = constrain((p_val + i_val + d_val) + (18000 / 2), 0, 18000);
+
+		/*Serial1.print("previousError: "); Serial1.println(previousError);
+		Serial1.print("error: "); Serial1.println(error);
+		Serial1.print("p_val: "); Serial1.println(p_val);
+		Serial1.print("i_val: "); Serial1.println(i_val);
+		Serial1.print("d_val: "); Serial1.println(d_val);
+		Serial1.print("controller_output: "); Serial1.println(controller_output);
+		Serial1.print("mapped_output: "); Serial1.println(centiDeg_to_us(controller_output, servoMin, servoMax));
+		Serial1.println();*/
+
+		status = true;
+		return centiDeg_to_us(controller_output, servoMin, servoMax);
+	}
+
+	status = false;
+	return 0;
+}
+
+
+
+
+float roll_controller::get_P_Component()
+{
+	return p_val;
+}
+
+
+
+
+float roll_controller::get_I_Component()
+{
+	return i_val;
+}
+
+
+
+
+float roll_controller::get_D_Component()
+{
+	return d_val;
+}
+
+
+
+
+float roll_controller::find_kp(float kp_,
+                               float ki_,
+                               float kd_,
+                               float samplePeriod)
+{
+	return (kp_ - (ki_ * samplePeriod)) * (samplePeriod - kd_);
+}
+
+
+
+
+float roll_controller::find_ki(float ki_, float samplePeriod)
+{
+	return ki_ * samplePeriod;
+}
+
+
+
+
+float roll_controller::p_component()
+{
+	return kp * error;
+}
+
+
+
+
+float roll_controller::i_component()
+{
+	summedError += ((error - previousError) / 2) * (sample_time_actual / 1000);
+	summedError = float_constrain(summedError, -i_limit, i_limit);
+
+	return ki * summedError;
+}
+
+
+
+
+float roll_controller::d_component()
 {
 	return kd * (error - previousError) / sample_time_actual;
 }
--- Autopilot.h
+++ Autopilot.h
@@ -9,8 +9,31 @@
 
 
 
-const float GRAVITY            = 9.80665; // m/s2
-const float AIRSPEED_THRESHOLD = 0.0001;  // m/s
+const float GRAVITY = 9.80665; // m/s2
+
+const int MAX_F = 20;
+const int MIN_F = 1;
+
+const float MAX_P = 10000;
+const float MIN_P = 0.1;
+
+const float MAX_I = 100000;
+const float MIN_I = 0.0;
+
+const float MAX_D = 10000;
+const float MIN_D = 0.0;
+
+const int MAX_RATE_UP = 100;
+const int MIN_RATE_UP = 0;
+
+const int MAX_RATE_DN = 100;
+const int MIN_RATE_DN = 0;
+
+const int MAX_I_LIMIT = 4500;
+const int MIN_I_LIMIT = 0;
+
+const int MAX_SERVO = 2000;
+const int MIN_SERVO = 1000;
 
 
 
@@ -18,6 +41,21 @@ const float AIRSPEED_THRESHOLD = 0.0001;  // m/s
 float float_constrain(float input, float min, float max);
 float float_map(float x, float in_min, float in_max, float out_min, float out_max);
 int centiDeg_to_us(int controller_output_, int servoMin_, int servoMax_);
+
+
+
+
+struct control_params {
+	float setpoint;   // Degrees
+	int maxRate_up;   // Degrees per Sec
+	int maxRate_down; // Degrees per Sec
+	float kp;         // Unitless
+	float ki;         // Unitless
+	float kd;         // Unitless
+	float sampleRate; // Hz
+	int servoMax;     // ms pulswidth
+	int servoMin;     // ms pulswidth
+};
 
 
 
@@ -30,28 +68,9 @@ public:
 
 
 
-	void begin(float setpoint_,    // Degrees
-	           int maxRate_up_,    // Degrees per Sec
-	           int maxRate_down_,  // Degrees per Sec
-	           float kp_,          // Unitless
-	           float ki_,          // Unitless
-	           float kd_,          // Unitless
-	           float roll_comp_,   // Unitless
-	           float sampleRate_,  // Hz
-	           int servoMax_,      // ms pulswidth
-	           int servoMin_);     // ms pulswidth
-
-	void update(float setpoint_,    // Degrees
-	            int maxRate_up_,    // Degrees per Sec
-	            int maxRate_down_,  // Degrees per Sec
-	            float kp_,          // Unitless
-	            float ki_,          // Unitless
-	            float kd_,          // Unitless
-	            float roll_comp_,   // Unitless
-	            float sampleRate_,  // Hz
-	            int servoMax_,      // ms pulswidth
-	            int servoMin_);     // ms pulswidth
-
+	void begin(control_params params);
+	void update(control_params params);
+	void reset();
 	float compute(float pitchAngle, // Degrees
 	              float rollAngle,  // Degrees
 	              float airspeed);  // m/s
@@ -67,33 +86,6 @@ private:
 	const float MAX_SETPOINT = 90.0;
 	const float MIN_SETPOINT = -90.0;
 
-	const float MAX_T = 1.0;
-	const float MIN_T = 0.4;
-
-	const float MAX_P = 3.0;
-	const float MIN_P = 0.1;
-
-	const float MAX_I = 0.5;
-	const float MIN_I = 0.0;
-
-	const float MAX_D = 0.2;
-	const float MIN_D = 0.0;
-
-	const int MAX_RATE_UP = 100;
-	const int MIN_RATE_UP = 0;
-
-	const int MAX_RATE_DN = 100;
-	const int MIN_RATE_DN = 0;
-
-	const float MAX_ROLL_COMP = 1.5;
-	const float MIN_ROLL_COMP = 0.7;
-
-	const int MAX_I_LIMIT = 4500;
-	const int MIN_I_LIMIT = 0;
-
-	const int MAX_SERVO = 2000;
-	const int MIN_SERVO = 1000;
-
 
 
 
@@ -103,14 +95,12 @@ private:
 
 	unsigned int samplePeriod_ms;
 	float samplePeriod_s;
+
 	float setpoint;
 	float kp;
 	float ki;
 	float kd;
 	float roll_comp;
-	float scaler_output   = 0;
-	float previous_scaler = 0;
-	int airspeed_scaler   = 1;
 
 	int servoMax;
 	int servoMin;
@@ -121,13 +111,10 @@ private:
 
 	int maxRate_up;
 	int maxRate_down;
-	int i_limit         = 1500; // Centi-Degrees
+	int i_limit         = 1000; // Centi-Degrees
 	float error         = 0;
 	float previousError = 0;
 	float summedError   = 0;
-	float biasedError   = 0;
-
-	float prevPitchAngle = 0;
 
 
 
@@ -137,9 +124,77 @@ private:
 	              float kd_,
 	              float samplePeriod_s);
 	float find_ki(float ki_, float samplePeriod_s);
-	float omega();
 	float roll_compensation(float rollAngle, float airspeed);
 	float find_as_scaler(float airspeed);
+	float p_component();
+	float i_component();
+	float d_component();
+};
+
+
+
+
+class roll_controller
+{
+public:
+	bool status = false;
+
+
+
+
+	void begin(control_params params);
+	void update(control_params params);
+	void reset();
+	float compute(float rollAngle,  // Degrees
+	              float airspeed);  // m/s
+
+	float get_P_Component();
+	float get_I_Component();
+	float get_D_Component();
+
+
+
+
+private:
+	const float MAX_SETPOINT = 180;
+	const float MIN_SETPOINT = -180;
+
+
+
+
+	unsigned long sampleTimer_previous = 0;
+	unsigned long sampleTimer_current  = 0;
+	unsigned long sample_time_actual   = 0;
+
+	unsigned int samplePeriod_ms;
+	float samplePeriod_s;
+
+	float setpoint;
+	float kp;
+	float ki;
+	float kd;
+
+	int servoMax;
+	int servoMin;
+
+	float p_val;
+	float i_val;
+	float d_val;
+
+	int i_limit = 1500; // Centi-Degrees
+	float error = 0;
+	float previousError = 0;
+	float summedError = 0;
+	float biasedError = 0;
+
+
+
+
+	float find_kp(float kp_,
+	              float ki_,
+	              float kd_,
+	              float samplePeriod_s);
+	float find_ki(float ki_, float samplePeriod_s);
 	float p_component();
 	float i_component();
 	float d_component();
