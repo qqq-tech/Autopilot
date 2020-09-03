beeb960dff635641dd50d7567befbb599011fe73
PB2 <gitstuff2@gmail.com>
2020-09-03 14:01:26 -0400

    Update fixed_wing.ino

--- examples/fixed_wing/fixed_wing.ino
+++ examples/fixed_wing/fixed_wing.ino
@@ -2,39 +2,101 @@
 #include "Autopilot.h"
 
 
-#define DEBUG_PORT    Serial2 //COM5
-#define FEEDBACK_PORT Serial5 //COM4
-
-const byte THROTTLE_PIN = 17;
-const byte PITCH_PIN    = 36;
-const byte ROLL_PIN     = 14;
-const byte YAW_PIN      = 16;
-
-int JOY_MAX = 1023;
-int JOY_MIN = 0;
-
-const unsigned int THROTTLE_MAX_ADC = 41060;
-const unsigned int THROTTLE_MIN_ADC = 24130;
-const unsigned int PITCH_MAX_ADC    = 41220;
-const unsigned int PITCH_MIN_ADC    = 23700;
-const unsigned int ROLL_MAX_ADC     = 41900;
-const unsigned int ROLL_MIN_ADC     = 25000;
-const unsigned int YAW_MAX_ADC      = 41700;
-const unsigned int YAW_MIN_ADC      = 24900;
+#define DEBUG_PORT    Serial1 //COM10
+#define FEEDBACK_PORT Serial2 //COM8
 
 
+////////////////////////////////////////////////////////////////////////// Constants
+const int JOY_MAX = 1023;
+const int JOY_MIN = 0;
+const int GEAR_TOGGLE_BUTTON = 1;
+const int FLAP_TOGGLE_BUTTON = 2;
+const int TAKEOFF_THRESH = 500;
+
+
+////////////////////////////////////////////////////////////////////////// objects
 SerialTransfer feedback;
-pitch_controller elevator;
+SerialTransfer datalog;
+
+pitch_controller pitchController;
+control_params pitchParams{
+  -10,  // setpoint
+  10,   // kp
+  0,    // ki
+  0,    // kd
+  10,   // sampleRate
+  1023, // outputMax
+  0     // ms pulswidth
+};
+
+roll_controller rollController;
+control_params rollParams{
+  0,    // setpoint
+  20,   // kp
+  0,    // ki
+  10,   // kd
+  10,   // sampleRate
+  1023, // outputMax
+  0     // ms pulswidth
+};
+
+heading_controller headingController;
+control_params headingParams{
+  40,   // setpoint
+  2,    // kp
+  0,    // ki
+  0,    // kd
+  10,   // sampleRate
+  35,   // outputMax
+  -35   // outputMin
+};
+
+altitude_controller altitudeController;
+control_params altitudeParams{
+  1000, // setpoint
+  0.1,  // kp
+  0,    // ki
+  0,    // kd
+  10,   // sampleRate
+  15,   // outputMax
+  -15   // outputMin
+};
+
+ias_controller iasController;
+control_params iasParams{
+  350,  // setpoint
+  10,   // kp
+  0,    // ki
+  0,    // kd
+  10,   // sampleRate
+  1023, // outputMax
+  0     // outputMin
+};
+
+state_params plane;
+
+struct extra_params{
+  float useWaypoints = 0;
+  float nextLat      = 0;
+  float nextLon      = 0;
+  float nextHdg      = 0;
+  float nextAlt      = 0;
+  float nextIAS      = 0;
+} extraParams;
+
+struct inputs{
+  float pitch = 0;
+  float roll  = 0;
+  float hdg   = 0;
+  float alt   = 0;
+  float ias   = 0;
+} setpoints;
 
 
-float pitch = 0;
-float roll  = 0;
-float ias   = 0;
-
-uint16_t pilot_throttle = 0;
-uint16_t pilot_pitch    = 512;
-uint16_t pilot_roll     = 512;
-uint16_t pilot_yaw      = 512;
+uint16_t throttle_command = JOY_MAX * 0.90;
+float pitch_command = 512;
+float roll_command  = 512;
+float yaw_command   = 512;
 
 
 ////////////////////////////////////////////////////////////////////////// setup()
@@ -42,74 +104,111 @@ void setup()
 {
   DEBUG_PORT.begin(115200);
   FEEDBACK_PORT.begin(115200);
-
+  
   feedback.begin(FEEDBACK_PORT);
-  elevator.begin(0.0,  // setpoint
-                 1,    // max rate up
-                 1,    // max rate down
-                 0.05, // kp
-                 0.0,  // ki
-                 0.0,  // kd
-                 0.7,  // roll compensation
-                 20.0, // sample rate
-                 JOY_MAX,  // servo max
-                 JOY_MIN); // servo min
+  datalog.begin(DEBUG_PORT);
 
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  setupControllers();
 
-  analogReadResolution(16);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 
 ////////////////////////////////////////////////////////////////////////// loop()
 void loop()
 {
-  getUserInputs();
+  if(handleData())
+    handleControllers();
   
-  if(getFeedback())
-  {
-    pilot_pitch = elevator.compute(pitch, roll, ias);
-  }
-
   sendJoyCommands();
 }
 
 
-void getUserInputs()
+////////////////////////////////////////////////////////////////////////// helping functions
+void setupControllers()
 {
-  pilot_throttle = constrain(map(analogRead(THROTTLE_PIN), THROTTLE_MIN_ADC, THROTTLE_MAX_ADC, JOY_MAX, JOY_MIN), JOY_MIN, JOY_MAX);
-  //pilot_pitch    = constrain(map(analogRead(PITCH_PIN),    PITCH_MIN_ADC,    PITCH_MAX_ADC,    JOY_MIN, JOY_MAX), JOY_MIN, JOY_MAX);
-  pilot_roll     = constrain(map(analogRead(ROLL_PIN),     ROLL_MIN_ADC,     ROLL_MAX_ADC,     JOY_MAX, JOY_MIN), JOY_MIN, JOY_MAX);
-  pilot_yaw      = constrain(map(analogRead(YAW_PIN),      YAW_MIN_ADC,      YAW_MAX_ADC,      JOY_MIN, JOY_MAX), JOY_MIN, JOY_MAX);
+  pitchController.begin(pitchParams);
+  rollController.begin(rollParams);
+  headingController.begin(headingParams);
+  altitudeController.begin(altitudeParams);
+  iasController.begin(iasParams);
 }
 
 
-bool getFeedback()
+bool handleData()
 {
-  if(feedback.available() && (feedback.bytesRead >= 6))
+  if(feedback.available())
   {
-    pitch = ((int16_t)(feedback.rxBuff[0] << 8) | feedback.rxBuff[1]) / 350.0;
-    roll  = ((int16_t)(feedback.rxBuff[2] << 8) | feedback.rxBuff[3]) / 350.0;
-    ias   = ((int16_t)(feedback.rxBuff[4] << 8) | feedback.rxBuff[5]) / 1000.0;
+    if(feedback.currentPacketID())
+    {
+      feedback.rxObj(setpoints);
+
+      if(!extraParams.useWaypoints)
+      {
+        altitudeController.setpoint = setpoints.alt;
+        iasController.setpoint      = setpoints.ias;
+      }
+    }
+    else
+    {
+      feedback.rxObj(extraParams, feedback.rxObj(plane));
+      plane.pitch = -plane.pitch;
+      plane.roll  = -plane.roll;
+
+      if(extraParams.useWaypoints)
+      {
+        headingController.setpoint  = find_heading(plane.lat, plane.lon, extraParams.nextLat, extraParams.nextLon);
+        altitudeController.setpoint = extraParams.nextAlt;
+        iasController.setpoint      = extraParams.nextIAS;
+      }
+      else
+        headingController.setpoint  = setpoints.hdg;
+
+      datalog.sendDatum(setpoints);;
+    }
 
     return true;
-  }
-  else if(feedback.status < 0)
-  {
-    DEBUG_PORT.print("ERROR: ");
-    DEBUG_PORT.println(feedback.status);
   }
 
   return false;
 }
 
 
+void handleControllers()
+{
+  float temp_pitch_command = pitchController.compute(plane);
+  if(pitchController.status)
+    pitch_command = temp_pitch_command;
+
+  float temp_roll_command  = rollController.compute(plane);
+  if(rollController.status)
+    roll_command = temp_roll_command;
+
+  float temp_heading_command  = -headingController.compute(plane);
+  if(headingController.status)
+    rollController.setpoint = temp_heading_command;
+
+  float temp_altitude_command  = -altitudeController.compute(plane);
+  if(altitudeController.status)
+    pitchController.setpoint = temp_altitude_command;
+
+  float temp_ias_command  = iasController.compute(plane);
+  if(iasController.status)
+    throttle_command = temp_ias_command;
+
+  setpoints.pitch = -pitchController.setpoint;
+  setpoints.roll  = -rollController.setpoint;
+  setpoints.hdg   = headingController.setpoint;
+  setpoints.alt   = altitudeController.setpoint;
+  setpoints.ias   = iasController.setpoint;
+}
+
+
 void sendJoyCommands()
 {
-  Joystick.X(pilot_pitch);          //roll
-  Joystick.Y(pilot_roll);           //pitch
-  Joystick.Z(pilot_yaw);            //yaw
-  Joystick.Zrotate(pilot_throttle); //throttle
-  Joystick.hat(-1);                 //hat
+  Joystick.X(pitch_command);
+  Joystick.Y(roll_command);
+  Joystick.Z(yaw_command);
+  Joystick.Zrotate(throttle_command);
 }
